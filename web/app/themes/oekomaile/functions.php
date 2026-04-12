34947040b028600195a24fa938f1700b46acd1ad
Kevin Lucas Simon <kevin.simon@grow-agency.de>
2026-04-12 23:11:12 +0200

    Add contributor page type

--- web/app/themes/oekomaile/functions.php
+++ web/app/themes/oekomaile/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 // activate plugin
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles', 11);
 function my_theme_enqueue_styles()
@@ -7,8 +8,30 @@ function my_theme_enqueue_styles()
 }
 
 // enable SVG support
-function wps_mime_types( $mimes ){
+function wps_mime_types($mimes)
+{
     $mimes['svg'] = 'image/svg+xml';
+
     return $mimes;
 }
-add_filter( 'upload_mimes', 'wps_mime_types' );
+add_filter('upload_mimes', 'wps_mime_types');
+
+// add custom post type
+function contributor_custom_post_type()
+{
+    register_post_type('contributor',
+        [
+            'labels' => [
+                'name' => __('Mitwirkende', 'oekomaile'),
+                'singular_name' => __('Mitwirkende:r', 'oekomaile'),
+            ],
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => ['slug' => 'mitwirkende'],
+            'show_in_rest' => true,
+            'hierarchical' => true,
+            'supports' => ['title', 'editor', 'thumbnail', 'excerpt', 'page-attributes'],
+        ]
+    );
+}
+add_action('init', 'contributor_custom_post_type');
